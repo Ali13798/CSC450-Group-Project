3534f81870afdde3689413c26b05fab48a48447e
Mackensie-B <mab2017@live.missouristate.edu>
2022-03-14 22:01:20 -0500

    Blocks any .com, but must be mani v2

--- StudyAssist/background.js
+++ StudyAssist/background.js
@@ -1,3 +1,13 @@
-chrome.runtime.onInstalled.addListener(() =>{
-    
+//Working on the whitlist aspect
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.webRequest.onBeforeRequest.addListener(
+        function () {
+            return {cancel:true};
+        },
+        {
+            urls: ["*://*.com/*"]
+        },
+        ["blocking"]
+    );
 });
