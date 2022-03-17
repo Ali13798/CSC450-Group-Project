bb97f59d66e6cafd5abe753819f2061ad3b8fdd7
Mackensie-B <mab2017@live.missouristate.edu>
2022-03-16 23:27:16 -0500

    Began attempt to use chrome.tabs for blocking

--- StudyAssist/background.js
+++ StudyAssist/background.js
@@ -11,3 +11,18 @@ chrome.runtime.onInstalled.addListener(() => {
         ["blocking"]
     );
 });
+// let wait = 1000;
+// async function checkCurrentTab() {
+//     let queryOptions = { active: true, currentWindow: true };
+//     let [tab] = await chrome.tabs.query(queryOptions);
+//     var tabsToRemove = [];
+//     if (tab){
+//         if (!tab.pendingUrl.includes(".edu")){
+//             tabsToRemove.push(tab.id)
+//             chrome.tabs.remove(tabsToRemove).then(function(){});
+//         }
+//     }else{
+//         console.log("none")
+//     }
+// }
+// let interval = setInterval(checkCurrentTab, wait);
