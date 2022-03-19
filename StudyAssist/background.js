199a2660ff2e16f8c7cfdaa173e390e76a07c14d
Mackensie-B <mab2017@live.missouristate.edu>
2022-03-18 23:48:25 -0500

    diff block approach: block by list

--- StudyAssist/background.js
+++ StudyAssist/background.js
@@ -1,46 +1,83 @@
-//Working on the whitlist aspect
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.local.get(["blocked", "enabled"], function (local) {
+      if (!Array.isArray(local.blocked)) {
+        chrome.storage.local.set({ blocked: [] });
+      }
+  
+      if (typeof local.enabled !== "boolean") {
+        chrome.storage.local.set({ enabled: false });
+      }
+    });
+});
+
+// chrome.runtime.onMessage.addListener((msg) => {
+//     if(msg.message == "break"){
+//         chrome.webRequest.onBeforeRequest.addListener(
+//             function () {
+//                 return {cancel:true};
+//             },
+//             {
+//                 urls: ["*://*.edu/*"]
+//             },
+//             ["blocking"]
+//         );
+//     }
+//     return true;  
+// });
 chrome.runtime.onMessage.addListener((msg) => {
-    // console.log("in background");
-    // console.log("about to block");
-    if(msg.message == "end"){
-        chrome.webRequest.onBeforeRequest.addListener(
-            function () {
-                return {cancel:true};
-            },
-            {
-                urls: ["*://*.edu/*"]
-            },
-            ["blocking"]
-        );
-    }else{
-        chrome.webRequest.onBeforeRequest.addListener(
-            function () {
-                return {cancel:true};
-            },
-            {
-                urls: ["*://*.com/*"]
-            },
-            ["blocking"]
-        );
-        
+    console.log("checked message recieved");
+    if(msg.message == "begin"){
+        if(!checkbox.checked){ //if not checked
+            checkbox.click() //then check it
+        }
+    }else if(msg.message == "end"){
+        if(checkbox.checked){ //if checked
+            checkbox.click() //then uncheck it
+        }
     }
     return true;  
 });
 
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+    const url = changeInfo.pendingUrl || changeInfo.url;
+    if (!url || !url.startsWith("http")) {
+      return;
+    }
+  
+    const hostname = new URL(url).hostname;
+  
+    chrome.storage.local.get(["blocked", "enabled"], function (local) {
+      const { blocked, enabled } = local;
+      if (Array.isArray(blocked) && enabled && blocked.find(domain => hostname.includes(domain))) {
+        chrome.tabs.remove(tabId);
+      }
+    });
+});
 
-//Mackensie - Research on alternative blocking methods: 
-// let wait = 1000;
-// async function checkCurrentTab() {
-//     let queryOptions = { active: true, currentWindow: true };
-//     let [tab] = await chrome.tabs.query(queryOptions);
-//     var tabsToRemove = [];
-//     if (tab){
-//         if (!tab.pendingUrl.includes(".edu")){
-//             tabsToRemove.push(tab.id)
-//             chrome.tabs.remove(tabsToRemove).then(function(){});
-//         }
-//     }else{
-//         console.log("none")
+//Working on the whitlist aspect
+// chrome.runtime.onMessage.addListener((msg) => {
+//     if(msg.message == "break"){
+//         chrome.webRequest.onBeforeRequest.addListener(
+//             function () {
+//                 return {cancel:true};
+//             },
+//             {
+//                 urls: ["*://*.edu/*"]
+//             },
+//             ["blocking"]
+//         );
+//     }else if (msg.message == "begin"){
+//         chrome.webRequest.onBeforeRequest.addListener(
+//             function () {
+//                 return {cancel:true};
+//             },
+//             {
+//                 urls: ["*://*.com/*"]
+//             },
+//             ["blocking"]
+//         );
+//     }else if(msg.message == "end"){
 //     }
-// }
-// let interval = setInterval(checkCurrentTab, wait);
+//     return true;  
+// });
--- StudyAssist/popup.js
+++ StudyAssist/popup.js
@@ -1,17 +1,60 @@
 //Clicking on the button starts the blocking session
 document.addEventListener("DOMContentLoaded", ()=>{
-    document.getElementById("beginSession").addEventListener('click', () => {
-        console.log("button clicked, start session");
-        chrome.runtime.sendMessage({message: "begin"}, function(response){
+    var textarea = document.getElementById("textarea");
+    var save = document.getElementById("save");
+    var checkbox = document.getElementById("checkbox");
 
-        });
+    document.getElementById("beginSession").addEventListener('click', () => {
+       if(!checkbox.checked){
+           checkbox.click();
+       }
     });
 
     document.getElementById("endSession").addEventListener('click', () => {
-        console.log("button clicked, end session");
-        chrome.runtime.sendMessage({message: "end"}, function(response){
-
-        });
+        if(checkbox.checked){
+            checkbox.click();
+        }
     });
 
+    var optionsBtn = document.getElementById("optionsBtn");
+    optionsBtn.addEventListener('click', () => {
+        console.log("optionsBtn clicked");
+        if(optionsBtn.innerHTML == "Options"){
+            //show the options
+            var options = document.getElementById("options");
+            options.style.display = "block";
+            //change options button to hide options
+            optionsBtn.innerHTML = "Hide Options";
+        }else{
+            //hide the options
+            var options = document.getElementById("options");
+            options.style.display = "none";
+            //change hide options button to options
+            optionsBtn.innerHTML = "Options";
+        }
+    });
+
+    //saves the list of blocked sites to localstorage
+    save.addEventListener("click", () => {
+        const blocked = textarea.value.split("\n").map(s => s.trim()).filter(Boolean);
+        chrome.storage.local.set({ blocked });
+    });
+    
+    //sets the user's choice of blocking to be enabled
+    checkbox.addEventListener("change", (event) => {
+        const enabled = event.target.checked;
+        chrome.storage.local.set({ enabled });
+    });
+    
+    window.addEventListener("DOMContentLoaded", () => {
+        //get the list of sites and the user's choice of enabled from local storage
+        chrome.storage.local.get(["blocked", "enabled"], function (local) {
+            const { blocked, enabled } = local;
+            if (Array.isArray(blocked)) {
+            textarea.value = blocked.join("\n");
+            checkbox.checked = enabled;
+            }
+        });
+    });
 });
+
