3a179ca9cdd21bb28861b5366b813b9a8dc2b8ae
Ali K <karimiafsharali@gmail.com>
2022-04-03 23:37:50 -0500

    add /home route to server

--- StudyAssist/server/app.py
+++ StudyAssist/server/app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Homepage")
+    return render_template("index.html", title="Index")
+
+@app.route("/home")
+def home():
+    return render_template("home.html", title="Homepage")
 
 
 def main():
