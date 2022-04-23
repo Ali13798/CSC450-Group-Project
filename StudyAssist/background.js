fcd61edc604b2c9f6af330993c2dbccfb525be9e
Mackensie-B <mab2017@live.missouristate.edu>
2022-04-23 15:09:23 -0500

    removed old function not using

--- StudyAssist/background.js
+++ StudyAssist/background.js
@@ -26,9 +26,3 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
     }
   });
 });
-
-async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
