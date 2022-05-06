fa3e9d83eeba28a0f2e51103dc6d845eb5cf1b6b
Mackensie-B <mab2017@live.missouristate.edu>
2022-05-06 17:36:04 -0500

    Completed history page

--- server/app.py
+++ server/app.py
@@ -13,7 +13,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-DB_PATH = "database.db"
+DB_PATH = "./server/database.db"
 
 
 @app.route("/")
@@ -68,24 +68,7 @@ def greet():
 @app.route("/greetNewUser", methods=["GET", "POST"])
 def new_user():
     name = flask.request.form.get("name")
-    if " " in name:
-        flask.flash('Username cannot contain empty spaces " ". Try again.')
-        return flask.redirect(flask.url_for("signup"))
-
-    if not name[0].isalpha():
-        flask.flash("Username must begin with an alphabetic character.")
-        return flask.redirect(flask.url_for("signup"))
-
-    if not name.isalnum():
-        flask.flash("Username cannot contain any special characters.")
-        return flask.redirect(flask.url_for("signup"))
-
     password = flask.request.form.get("password")
-    confirm_password = flask.request.form.get("confirm_password")
-    if password != confirm_password:
-        flask.flash("Passwords entered do not match. Try again.")
-        return flask.redirect(flask.url_for("signup"))
-
     password = hash_password(password)
 
     with sqlite3.connect(DB_PATH) as con:
