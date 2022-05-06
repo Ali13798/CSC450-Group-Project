b69cedfadd6e79fa24ef0daa3372259cad035280
Ali K <karimiafsharali@gmail.com>
2022-05-06 17:08:33 -0500

    make /history read from database

--- server/app.py
+++ server/app.py
@@ -188,32 +188,48 @@ def stats():
 def history():
     # if you can send all of them, I can have the user provide a date
     # range so they have the ability to see all of their history as our teacher asked
-    if not flask.session.get("name"):
+    username = flask.session.get("name")
+    if not username:
         return flask.redirect("/")
 
-    userHist: list[dict[str, int]] = [
-        {
-            "id": 1,
-            "date": "5/1/22",
-            "clickCount": 5,
-            "keyCount": 20,
-            "timeStudied": 125,
-        },
-        {
-            "id": 2,
-            "date": "5/2/22",
-            "clickCount": 41,
-            "keyCount": 12,
-            "timeStudied": 60,
-        },
-        {
-            "id": 3,
-            "date": "5/3/22",
-            "clickCount": 100,
-            "keyCount": 80,
-            "timeStudied": 310,
-        },
-    ]
+    with sqlite3.connect(DB_PATH) as con:
+        cur = con.cursor()
+        user_stats = db_tools.get_stats(cur=cur, username=username)
+
+    userHist: list[dict[str, int]] = []
+    for stat in user_stats:
+        temp_dict = {
+            "id": stat[0],
+            "date": stat[2],
+            "timeStudied": stat[3],
+            "clickCount": stat[4],
+            "keyCount": stat[5],
+        }
+        userHist.append(temp_dict)
+
+    # userHist: list[dict[str, int]] = [
+    #     {
+    #         "id": 1,
+    #         "date": "5/1/22",
+    #         "clickCount": 5,
+    #         "keyCount": 20,
+    #         "timeStudied": 125,
+    #     },
+    #     {
+    #         "id": 2,
+    #         "date": "5/2/22",
+    #         "clickCount": 41,
+    #         "keyCount": 12,
+    #         "timeStudied": 60,
+    #     },
+    #     {
+    #         "id": 3,
+    #         "date": "5/3/22",
+    #         "clickCount": 100,
+    #         "keyCount": 80,
+    #         "timeStudied": 310,
+    #     },
+    # ]
 
     return render_template("history.html", title="History", userHist=userHist)
 
