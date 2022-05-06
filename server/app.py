0ed5344ada5aeed0ab0d0aad0f9df5a9785a9df6
Ali K <karimiafsharali@gmail.com>
2022-05-06 16:25:32 -0500

    correct db path

--- server/app.py
+++ server/app.py
@@ -13,7 +13,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-DB_PATH = "database.db"
+DB_PATH = "./server/database.db"
 
 
 @app.route("/")
